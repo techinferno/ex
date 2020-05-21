97aa6a367fd08dd651d32bfef4fcb519eb75fa89
Ayush Raj Choudhary <46986779+ayushrjc@users.noreply.github.com>
2020-05-21 13:15:44 +0530

    new file to upload

---
+++ TurtleGraphics/t1.py
@@ -0,0 +1,81 @@
+### BETTER RUN IN TERMINAL ###
+import turtle as t
+# t.goto(-100,-100)
+#
+# t.penup()
+# t.goto(50,-50)
+#
+# t.pendown()
+# t.goto(150,130)
+#
+# t.color('blue')
+# t.goto(250,80)
+#
+# t.pensize(20)
+# t.goto(250,80)
+#
+# t.speed((50))
+# t.goto(150,130)
+#
+# t.getscreen().bgcolor('blue')
+# t.goto(250,80)
+#
+# t.forward(300)
+# t.speed(1)
+# t.forward(300)
+# t.left(90)
+# t.forward(130)
+# t.right(90)
+# t.forward(300)
+
+
+## Creating Figure ###
+#
+# t.pendown()
+# t.penup()
+# t.goto(-250,250)
+# t.forward(100)
+# t.pendown()
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+#
+#
+# ## Same with for loop
+# for i in range(1,5):
+#     t.forward(200)
+#     t.right(90)
+
+## Creating a circle##
+# t.penup()
+# t.goto(0,0)
+# t.pendown()
+# t.color('red')
+# t.circle(200)
+
+# ## Circle with loop
+# t.goto(20,-30)
+# t.color('blue')
+# t.speed(100)
+# for i in range(1,100):
+#     t.circle(i)
+
+#
+# t.goto(20,-30)
+# t.color('red')
+# t.speed(100)
+# for i in range(1,100):
+#     t.circle(i+50)
+#     t.left(10)
+#
+# ## Creating a STAR
+# t.goto(150,-250)
+# for i in range(1,10):
+#     t.forward(150)
+#     t.left(135)
+
+
